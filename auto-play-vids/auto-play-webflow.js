56854a8f5395082c92bbfe06c42cb4699eded5a2
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2024-11-09 09:53:48 -0500

    add short volume ramp to mute on and off

--- auto-play-vids/auto-play-webflow.js
+++ auto-play-vids/auto-play-webflow.js
@@ -3,23 +3,62 @@
 // <video loop="true" playsinline>
 
 
+
+function adjustVolume(video, targetVolume, duration = 500, ramping) {
+    // Unmute if ramping up the volume
+    if (targetVolume > 0) { 
+    	video.muted = false;
+      ramping = true;
+    }
+    
+    const startVolume = video.volume;
+    const volumeDifference = targetVolume - startVolume;
+    const stepTime = 50; // Time interval for each step in ms
+    const steps = duration / stepTime;
+    let currentStep = 0;
+    
+    const volumeInterval = setInterval(() => {
+        currentStep++;
+        const progress = currentStep / steps;
+        video.volume = Math.pow( startVolume + volumeDifference * progress, 2);
+        
+        if (currentStep >= steps) {
+            clearInterval(volumeInterval);
+            if (targetVolume === 0) {
+            	video.muted = true; // Mute after ramping down
+            	ramping = false;
+            }
+        }
+    }, stepTime);
+}
+
+
 Webflow.push( function() {
 	const movies = document.querySelectorAll(".video-loop");
-
+	let volRamping = false;
+  
   movies.forEach( (item) => {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach(entry => {
           function handleClick( event ) {
-            item.muted = !item.muted;
+            if( !volRamping ) {
+            	if( item.muted ) {
+                adjustVolume( item, 1, 500, volRamping );
+              } else {
+                adjustVolume( item, 0, 500, volRamping );
+              }
+            }
           }
           
           if (entry.isIntersecting) {
-          		console.log('entry?: ', entry);
+          		
               // make sure muted
               item.muted = true;
+              item.volume = 0;
               
               // trigger play
               item.play();
+              
              
               // add event listener
               if(!item.hasAttribute('data-click-added') ) {
@@ -47,4 +86,3 @@ Webflow.push( function() {
       observer.observe(item);
   });
 })
-
